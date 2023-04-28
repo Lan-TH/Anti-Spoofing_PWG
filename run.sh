3e0344bf85b2705573b8d31aefe230a82204f82d
Lan-TH <74036742+Lan-TH@users.noreply.github.com>
2023-04-28 17:29:26 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,70 @@
+# extract features
+RootDir=`pwd`
+echo 'Current Dir: '${RootDir}
+
+extractLPS=1
+if [ $extractLPS -eq 1 ]; then
+    echo 'Extracting Log Power Spectrograms ...'
+    python ./preprocess/compute_LPS.py --in_dir ./_saved/models/LA_SENet12_LPSseg_uf_seg600/20221204_192106/noadv_cocoder_wav \
+                                       --out_dir ./_saved/models/LA_SENet12_LPSseg_uf_seg600/20221204_192106/noadv_cocoder_wav_feature \
+                                       --param_json_path ./preprocess/conf/stft_T45.json
+fi
+
+
+extractLMS=0
+if [ $extractLMS -eq 1 ]; then
+    echo 'Extracting Log Magnitude Spectrograms ...'
+    python ./preprocess/compute_LMS.py --in_dir /home/turing/data2/tianhao/asvspoof2019/data_logical/ \
+                                       --out_dir ./data/LMS \
+                                       --access_type PA \
+                                       --param_json_path ./preprocess/conf/stft.json
+fi
+
+
+# train
+GPU=1
+python develop.py  --resume _saved/models/LA_lcnn_LPSseg_uf_seg600/20221204_192106/model_best.pth \
+                    --protocol_file ~/asvspoof2019/data_logical/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.dev.trl.txt \
+                    --asv_score_file ~/asvspoof2019/data_logical/LA/ASVspoof2019_LA_asv_scores/ASVspoof2019.LA.asv.dev.gi.trl.scores.txt \
+                    --device ${GPU}    
+
+
+# test
+python eval.py    --resume _saved/models/LA_SENet12_LPSseg_uf_seg600/20221204_192106/checkpoint-epoch10.pth \
+                  --protocol_file ~/asvspoof2019/data_logical/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt \
+                  --asv_score_file ~/asvspoof2019/data_logical/LA/ASVspoof2019_LA_asv_scores/ASVspoof2019.LA.asv.eval.gi.trl.scores.txt \
+                  --device ${GPU}
+
+
+# attack
+python pgd_attack.py  --resume _saved/models/LA_SENet12_LPSseg_uf_seg600/20221204_192106/model_best.pth \
+                  		--protocol_file ~/asvspoof2019/data_logical/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt \
+                  		--asv_score_file ~/asvspoof2019/data_logical/LA/ASVspoof2019_LA_asv_scores/ASVspoof2019.LA.asv.eval.gi.trl.scores.txt \
+                  		--device ${GPU}
+
+
+# test
+python eval_adv.py     --resume _saved/models/LA_SENet12_LPSseg_uf_seg600/20221204_192106/model_best.pth \
+          						 --adv_data _saved/models/LA_SENet12_LPSseg_uf_seg600/20221204_192106/fgsm_adv_egs_None_${epsilon} \
+ 		                   --protocol_file ~/asvspoof2019/data_logical/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt \
+ 		                   --asv_score_file ~/asvspoof2019/data_logical/LA/ASVspoof2019_LA_asv_scores/ASVspoof2019.LA.asv.eval.gi.trl.scores.txt \
+ 		                   --device ${GPU}	
+
+
+
+
+
+# generate waveform
+data_dir=~/_saved/models/LA_SENet12_LPSseg_uf_seg600/20221204_192106/pgd_adv_egs_None_5.0_all_wav/
+    parallel-wavegan-decode \
+        --checkpoint pretrained_model/train_nodev_ljspeech_parallel_wavegan.v1.long/checkpoint-1000000steps.pkl \
+        --feats-scp ./feats.scp \
+        --normalize-before \
+        --outdir $data_dir/generated_wav
+
+
+# spectral maps
+python spectral_maps.py
+
+# waveform
+python waveform.py
